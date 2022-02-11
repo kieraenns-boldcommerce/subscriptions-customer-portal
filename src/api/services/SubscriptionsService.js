7cbbde19a0b30821e5a63f9de1615cb33198924e
aka <aka@outsi.de>
2022-02-11 18:41:34 +0300

    test: refresh token

--- src/api/services/SubscriptionsService.js
+++ src/api/services/SubscriptionsService.js
@@ -5,7 +5,55 @@ import AddressesAdapter from "../adapters/AddressesAdapter";
 import IntervalsAdapter from "../adapters/IntervalsAdapter";
 import PaymentMethodsAdapter from "../adapters/PaymentMethodsAdapter";
 
+const updateCookieToken = (token, expTime) => {
+  console.log(" > expTime", expTime);
+  Cookies.set(Cookie.TOKEN, token, {expires: expTime});
+  setTimeout(async () => {
+    const {
+      newToken,
+      newTokenExpTime
+    } = await this.callAPI({
+      method: Method.GET,
+      url: "/jwt/refresh",
+      params: {
+        shop: SHOP_DOMAIN,
+        platform_type: PLATFORM
+      }
+    });
+    console.log(" > newTokenExpTime", newTokenExpTime);
+    updateCookieToken(newToken, newTokenExpTime);
+  }, 1000 * 10); // refresh token in 10 seconds
+  // }, tokenExpTime - 1000 * 60 * 5); // refresh token 5 minutes before expiring
+};
+
 class SubscriptionsService extends ServiceBase {
+  static async obtainToken() {
+    if (!Cookies.get(Cookie.TOKEN) || Cookies.get(Cookie.CUSTOMER_ID)) {
+      const {
+        value: {jwt: platformToken, customerId: platformCustomerID}
+      } = await new Promise((resolve) => window.BOLD.subscriptions.getJWT(resolve));
+
+      const {
+        subscriptionsWebToken: boldToken,
+        subscriptionsWebTokenExpTime: tokenExpTime,
+        customer: {bold_platform_id: boldCustomerID}
+      } = await this.callAPI({
+        method: Method.GET,
+        url: "/login",
+        params: {
+          platform_customer_id: platformCustomerID,
+          customer_jwt: platformToken,
+          shop: SHOP_DOMAIN,
+          platform_type: PLATFORM
+        }
+      });
+
+      Cookies.set(Cookie.CUSTOMER_ID, boldCustomerID);
+
+      updateCookieToken(boldToken, tokenExpTime);
+    }
+  }
+
   static async getSubscriptions() {
     const {
       value: {jwt: platformToken, customerId: platformCustomerID}
