f6c33b6aac5c9e7e359ac0097c97fc2edc766a7e
aka <aka@outsi.de>
2022-02-17 13:51:39 +0300

    fix: build

--- src/components/PaymentMethodForm.jsx
+++ src/components/PaymentMethodForm.jsx
@@ -1,26 +1,24 @@
-import {useContext} from "react";
-import {AppStateContext} from "../AppState";
-import {Button} from "react-query/types/devtools/styledComponents";
+import { useContext } from "react";
+import { AppStateContext } from "../AppState";
+import { Button } from "@boldcommerce/stacks-ui";
 
 const PaymentMethodForm = () => {
   const { appState, appActions } = useContext(AppStateContext);
 
-  const {
-    paymentMethod
-  } = appState;
+  const { paymentMethod } = appState;
 
-  const {
-    stopUpdatePaymentMethod,
-    finishUpdatePaymentMethod
-  } = appActions;
+  const { stopUpdatePaymentMethod, finishUpdatePaymentMethod } = appActions;
 
-  const handleConfirmButtonClick = () => finishUpdatePaymentMethod(paymentMethod.updateMethod);
+  const handleConfirmButtonClick = () =>
+    finishUpdatePaymentMethod(paymentMethod.updateMethod);
   const handleCancelButtonClick = () => stopUpdatePaymentMethod();
 
-  return <>
-    <Button onClick={handleConfirmButtonClick}>Send email</Button>
-    <Button onClick={handleCancelButtonClick}>Cancel</Button>
-  </>;
+  return (
+    <>
+      <Button onClick={handleConfirmButtonClick}>Send email</Button>
+      <Button onClick={handleCancelButtonClick}>Cancel</Button>
+    </>
+  );
 };
 
 export default PaymentMethodForm;
